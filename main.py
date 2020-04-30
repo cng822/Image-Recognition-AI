8c9fb769a6b3a313a367b92bdfd65d7e0fc86cf1
maheknangia <63040474+maheknangia@users.noreply.github.com>
2020-04-30 12:22:56 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,117 +1,92 @@
+from __future__ import print_function
 import torch
-import torchvision
-import torch.nn as nn
-import matplotlib.pyplot as plt
 import torch.optim as optim
-from algorithms.alexnet import AlexNet
-from algorithms.vgg import VGG
-from algorithms.lenet import LeNet
-from torchvision import transforms, datasets
-import matplotlib.pyplot as plt
+import torchvision
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
 import numpy as np
+import torch.nn.functional as F
+from PIL import Image
+from cnn import Net
 
-epoches = 5
-start_epoch = 1
-batchsize = 10
-learning_rate = 0.001
-transform = transforms.Compose([ transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-train_CIFAR10 = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform= transform )
-
-test_CIFAR10 = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                        download=True, transform= transform)
-
-train_loader = torch.utils.data.DataLoader(train_CIFAR10, batch_size = batchsize, shuffle = True)# batch_size to change
-
-test_loader = torch.utils.data.DataLoader(test_CIFAR10, batch_size = batchsize, shuffle = True)# batch_size to change
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-kwargs = {'num_workers': 1, 'pin_memory': True} if device else {}
-#model = AlexNet()
-model = LeNet()
-model = VGG()
-model = model.to(device)
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+def imsave(img):
     npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+    npimg = (np.transpose(npimg, (1, 2, 0)) * 255).astype(np.uint8)
+    im = Image.fromarray(npimg)
+    im.save("./results/your_file.jpeg")
 
-# get some random training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-    # print labels
-# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr= learning_rate)
-
-def train(epoch):
+def train_cnn(log_interval, model, device, train_loader, optimizer, epoch):
     model.train()
-    for batch_no, (data, target) in enumerate(train_loader):
+    for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
+        loss = F.nll_loss(output, target)
+        loss.backward(); optimizer.step()
+        if batch_idx % log_interval == 0:
+            print ('Train Epoch: {} [{}/{} (:.0f)%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx*len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader)
+                                                                       , loss.item()))
 
-        if batch_no % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_no * len(data), len(train_loader.dataset),
-                100. * batch_no / len(train_loader), loss.item()))
-
-
-def test(epoch):
+def test(model, device, test_loader):
     model.eval()
-    correct = 0
-    total = 0
     test_loss = 0
-
+    correct = 0
     with torch.no_grad():
         for data, target in test_loader:
+            data = torch.squeeze(data)
             data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
             output = model(data)
-            loss = criterion(output, target)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset())
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
 
-            test_loss += loss.item()
-            _, predicted = output.max(1)
-            total += target.size(0)
-            correct += predicted.eq(target).sum().item()
+def main():
+    epoches = 14
+    gamma = 0.7
+    log_interval = 10
+    torch.manual_seed(1)
+    save_model = True
 
-        test_loss /= len(test_loader.dataset)
+    RNN = False # Check
+    N_STEPS = 28
+    N_INPUTS = 28
+    N_NEURONS = 150
+    N_OUTPUTS = 10
 
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
-        # # Save checkpoint.
-        # acc = 100. * correct / total
-        # if acc > best_acc:
-        #     print('Saving..')
-        #     state = {
-        #         'net': net.state_dict(),
-        #         'acc': acc,
-        #         'epoch': epoch,
-        #     }
-        #     torch.save(state, './checkpoint/ckpt.pth')
-        #     best_acc = acc
-#
-# def adjust_learning_rate(optimizer, epoch):
-#     global state
-#     if epoch in args.schedule:
-#         state['lr'] *= args.gamma
-#         for param_group in optimizer.param_groups:
-#             param_group['lr'] = state['lr']
-#
-for epoch in range(start_epoch, epoches + 1):
-   train(epoch)
-   test(epoch)
+    device = torch.device("cpu") # Do not have cuda on laptop
+
+    #Torchvision
+
+    kwargs = {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../data', train=True, download=True,
+                         transform=transforms.Compose([
+                             transforms.ToTensor()])), batch_size=64, shuffle=True, **kwargs)
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../data', train=False, transform=transforms.Compose([
+                             transforms.ToTensor()])), batch_size=1000, shuffle=True, **kwargs)
+
+    dataiter = iter(train_loader)
+    images, labels = dataiter.next()
+    #check img = torchvision.utils.make_grid(images)
+    #check imsave(img)
+
+    model = Net().to(device)
+    optimizer = optim.Adam(model.parameters(), lr= 0.001)
+    scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+
+    for epoch in range(1, epoches + 1):
+        train_cnn(log_interval, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+
+    if save_model:
+        torch.save(model.state_dict, "./results/cifar_cnn.pt")
+
+if __name__ == '__main__':
+    main()
