47188b28d1e5259d885a0bc984a1071db54fce99
cng822 <50128761+cng822@users.noreply.github.com>
2020-05-12 20:50:03 +1200

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import torch.optim as optim
-from models.VGG import VGG
+from models.VGG16 import VGG16
 from models.ResNet import ResNet, BasicBlock
 from models.AlexNet import AlexNet
-from models.DenseNet import DenseNet, Bottleneck
+from models.SENet import SENet, BasicBlock 
 from torch.optim.lr_scheduler import StepLR
 from torchvision import transforms, datasets
 import numpy as np
 
 epoches = 10
 start_epoch = 1
-batchsize = 10
 learning_rate = 0.001
 actual = []
 prediction = []
 loss_graph = []
 accuracy_graph = []
+accuracy_train = []
+loss_test = []
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 train_CIFAR10 = torchvision.datasets.CIFAR10(root='./data', train=True,
@@ -28,9 +29,9 @@ train_CIFAR10 = torchvision.datasets.CIFAR10(root='./data', train=True,
 test_CIFAR10 = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform= transform)
 
-train_loader = torch.utils.data.DataLoader(train_CIFAR10, batch_size = batchsize, shuffle = True)# batch_size to change
+train_loader = torch.utils.data.DataLoader(train_CIFAR10, batch_size = 32, shuffle = True)# batch_size to change
 
-test_loader = torch.utils.data.DataLoader(test_CIFAR10, batch_size = batchsize, shuffle = True)# batch_size to change
+test_loader = torch.utils.data.DataLoader(test_CIFAR10, batch_size = 64, shuffle = True)# batch_size to change
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -39,11 +40,26 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if device else {}
 
+print("Please enter the number of one of the following models: \n AlexNet : 1 \n ResNet50 : 2 \n SENet : 3 \n VGG16 : 4")
+val = input("Enter here: ")
+found = 0
+while found != 1:
+    if val == "1":
+        model = AlexNet()
+        found = 1
+    elif val == "2":
+        model = ResNet(BasicBlock, [3, 4, 6, 3])
+        found = 1
+    elif val == "3":
+        model = SENet(BasicBlock, [2, 2, 2, 2])
+        found = 1
+    elif val == "4":
+        model = VGG()
+        found = 1
+    else:
+        val = input("Please enter again: ")
 
-# model = AlexNet()
-# model = ResNet(BasicBlock, [3, 4, 6, 3])
-#model = DenseNet(Bottleneck, [6, 12, 48, 32], growth_rate=32)
-model = VGG()
+
 model = model.to(device)
 
 
@@ -64,14 +80,15 @@ images, labels = dataiter.next()
 # print(' '.join('%5s' % classes[labels[j]] for j in range(10)))
 
 criterion = nn.CrossEntropyLoss()
-# optimizer = optim.Adam(model.parameters(), lr= learning_rate)
-optimizer = optim.SGD(model.parameters(), lr= learning_rate, momentum = 0.9)
-scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
+optimizer = optim.Adam(model.parameters(), lr= learning_rate)
+# optimizer = optim.SGD(model.parameters(), lr= learning_rate, momentum = 0.9)
+scheduler = StepLR(optimizer, step_size=1, gamma= 0.7)
 
 
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
-    loss_tol = 0;
+    loss_tol = 0
+    correct = 0
     for batch_no, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -79,12 +96,15 @@ def train(model, device, train_loader, optimizer, epoch):
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-        loss_tol += loss.item() * images.size(0)
+        loss_tol += loss.item()
+        _, predicted = output.max(1)
+        correct += predicted.eq(target).sum().item()
         if batch_no % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_no * len(data), len(train_loader.dataset),
                 100. * batch_no / len(train_loader), loss.item()))
     loss_graph.append(loss_tol / len(train_loader.dataset))
+    accuracy_train.append(correct / len(train_loader.dataset))
 
 
 def test(model, device, test_loader):
@@ -99,13 +119,13 @@ def test(model, device, test_loader):
             loss = criterion(output, target)
             test_loss += loss.item()
             _, predicted = output.max(1)
-            #total += target.size(0)
             actual.extend(target.view_as(predicted))
             prediction.extend(predicted)
             correct += predicted.eq(target).sum().item()
-
+        tol_loss = test_loss
         test_loss /= len(test_loader.dataset)
         accuracy_graph.append(correct / len(test_loader.dataset))
+        loss_test.append(tol_loss / len(test_loader.dataset))
         print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
             test_loss, correct, len(test_loader.dataset),
             100. * correct / len(test_loader.dataset)))
@@ -121,14 +141,17 @@ print('Confusion matrix:')
 print(metrics.confusion_matrix(actual, prediction))
 print(metrics.classification_report(actual, prediction, digits=3))
 
-plt.subplot(2,1,1,)
-plt.plot(loss_graph)
-plt.xlabel('training batch')
-plt.ylabel('loss')
+plt.subplot(211)
+plt.plot(loss_graph, label="training")
+plt.plot(loss_test, label="testing")
+plt.xlabel('Number of epochs')
+plt.ylabel('Loss')
+plt.legend(loc="upper right")
 
-plt.subplot(2,1,2)
-plt.plot(accuracy_graph)
-plt.xlabel('training batch')
-plt.ylabel('accuracy')
+plt.subplot(212)
+plt.plot(accuracy_train, label="training")
+plt.plot(accuracy_graph, label="testing")
+plt.xlabel('Number of epochs')
+plt.ylabel('Accuracy(%)')
+plt.legend(loc="upper right")
 plt.show()
-
